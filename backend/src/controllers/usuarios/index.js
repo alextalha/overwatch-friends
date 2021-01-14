d1c2de46a5a97f7827197f9f6765984254bb6a76
Alex Talha <alex.talha@original.io>
2021-01-14 19:17:51 -0300

    finalizado crud de usuario

--- backend/src/controllers/usuarios/index.js
+++ backend/src/controllers/usuarios/index.js
@@ -1,3 +1,5 @@
+
+const { response } = require('express');
 const knex = require('./../../database')
 
 
@@ -5,5 +7,71 @@ module.exports = {
 
      getAllUsers(request,response){
        knex('usuarios').then(results =>  response.json(results));     
-    } 
+    },
+
+    async getUser(request,response, next){
+            const {id} = request.params
+
+            console.log('id =======>', id)
+        try{
+            const user = await knex('usuarios')
+            .where({id})
+
+            return response.json(user)
+
+        }catch(error){
+            next(error)
+        }
+    },
+
+    async createUser(request,response,next){
+        try{
+            const { email , battletag, plataforma } = request.body
+        
+            await knex('usuarios').insert({
+                email,
+                battletag,
+                plataforma
+             })
+        
+            return response.status(201).send()
+
+        }catch(error){
+            next(error)
+        }
+           
+    },
+
+    async updateUser(request,response,next){
+        try{
+            const {email} = request.body
+            const {id} = request.params
+
+            await knex('usuarios')
+            .update({email})
+            .where({id})
+
+            return response.send();
+
+        }catch(error){
+            next(error)
+        }
+
+    },
+
+    async deleteUser(request,response,next){
+
+        try{
+            const { id } = request.params
+            await knex('usuarios')
+            .where({id})
+            .del()
+
+            return response.send();
+            
+        }catch(error){
+            next(error)
+        }
+
+    }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,10 +4,17 @@ const knex = require('./database')
 
 const routes = Router();
 const { stats, complete,specific_hero } = require('./controllers/owapi');
-const { getAllUsers } = require('./controllers/usuarios');
+const { getAllUsers, getUser, createUser, updateUser, deleteUser } = require('./controllers/usuarios');
 
 // usuarios 
 routes.get('/users', getAllUsers );
+routes.get('/user/:id', getUser );
+routes.post('/user/create', createUser)
+routes.put('/user/update/:id', updateUser)
+routes.put('/user/update/:id', updateUser)
+routes.delete('/user/delete/:id', deleteUser)
+
+
 
 // owapi
 routes.get('/overwatch/stats/:plataform/:region/:battletag', stats);
